0e9ec62bfe4cd79251f59779ad301a38502be3b8
Ruslan Polutsygan <ruslan.polutsygan@gmail.com>
2016-09-15 17:50:55 +0300

    barcode generated

--- server/api/ticket/ticket.controller.js
+++ server/api/ticket/ticket.controller.js
@@ -3,6 +3,8 @@
 import Ticket from './ticket.model';
 import * as mailer from '../../mailer/mailer.js';
 import * as crypto from 'crypto';
+import * as barcode from 'bwip-js';
+import * as fs from 'fs';
 
 function respondWithResult(res, statusCode) {
     statusCode = statusCode || 200;
@@ -41,7 +43,7 @@ export function buy(req, res) {
         .then(handleEntityNotFound(res))
         .then(ticket => {
             ticket.available = false;
-            ticket.code = crypto.randomBytes(64).toString('base64');
+            ticket.code = crypto.randomBytes(16).toString('hex');
 
 
 
@@ -58,15 +60,34 @@ export function buy(req, res) {
         .catch(handleError(res));
 }
 
-export function print(req, res) {
+export function print(req, res, next) {
     return Ticket.findOne({code: req.params.code, available: false}).exec()
         .then(handleEntityNotFound(res))
         .then((ticket) => {
             if(ticket) {
-                return res.render('ticket/print', {
-                    ticket: ticket,
-                    barcode: 'barcode1',
-                })
+
+                barcode.toBuffer({
+                    bcid:        'code128',       // Barcode type
+                    text:        ticket.code,     // Text to encode
+                    scale:       3,               // 3x scaling factor
+                    height:      10,              // Bar height, in millimeters
+                    includetext: true,            // Show human-readable text
+                    textxalign:  'center',        // Always good to set this
+                    textsize:    13               // Font size, in points
+                }, function (err, png) {
+                    // png is a Buffer. can be saved into file if needed  fs.writeFile(ticket._id + '.png', png)
+
+                    if (err) {
+                        return res.status(500).send('Could not generate ticket');
+                    } else {
+
+                        return res.render('ticket/print', {
+                            ticket: ticket,
+                            barcodeUri: png.toString('base64'),
+                        })
+
+                    }
+                });
             }
         })
         .catch(handleError(res));
