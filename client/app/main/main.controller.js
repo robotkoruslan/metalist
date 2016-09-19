9f628a654e2efc591ba7074df9f60799c3c867e3
Ruslan Polutsygan <ruslan.polutsygan@gmail.com>
2016-09-19 16:39:00 +0300

    "things" removed

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,27 +6,6 @@
 
         constructor($http) {
             this.$http = $http;
-            this.awesomeThings = [];
-        }
-
-        $onInit() {
-            this.$http.get('/api/things')
-                .then(response => {
-                    this.awesomeThings = response.data;
-                });
-        }
-
-        addThing() {
-            if (this.newThing) {
-                this.$http.post('/api/things', {
-                    name: this.newThing
-                });
-                this.newThing = '';
-            }
-        }
-
-        deleteThing(thing) {
-            this.$http.delete('/api/things/' + thing._id);
         }
     }
 
--- server/routes.js
+++ server/routes.js
@@ -9,7 +9,6 @@ import path from 'path';
 
 export default function (app) {
     // Insert routes below
-    app.use('/api/things', require('./api/thing'));
     app.use('/api/users', require('./api/user'));
     app.use('/api/tickets', require('./api/ticket'));
     app.use('/payment/liqpay', require('./api/payment/liqpay'));
