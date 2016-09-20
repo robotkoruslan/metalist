90b2d3de5fbf760ce9a72381073bab43dd471b08
Ruslan Polutsygan <ruslan.polutsygan@gmail.com>
2016-09-20 17:37:35 +0300

    path to matchseats template fixed

--- client/app/match-seats/match-seats.js
+++ client/app/match-seats/match-seats.js
@@ -4,7 +4,7 @@ angular.module('metallistTicketsApp')
     .config(function ($stateProvider) {
         $stateProvider.state('match-seats', {
             url: '/match/:id/seats',
-            templateUrl: 'app/tickets/match-seats.html',
+            templateUrl: 'app/match-seats/match-seats.html',
             controller: 'MatchSeatsController',
             controllerAs: 'controller',
 
