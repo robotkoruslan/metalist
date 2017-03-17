28ad8c195460002e782c8c1013f58f67b9b0d3c8
Sash730 <i_730@ukr.net>
2017-03-17 23:16:01 +0200

    price for match

--- client/app/stadium/stadium.controller.js
+++ client/app/stadium/stadium.controller.js
@@ -10,7 +10,7 @@
         {color: '#ff972f', colorName: 'green', price: '10'},
         {color: '#ffcc00', colorName: 'violet', price: '20'},
         {color: '#54aa6a', colorName: 'yellow', price: '30'},
-        {color: '#6f89c0', colorName: 'blue', price: '500'},
+        {color: '#6f89c0', colorName: 'blue', price: '40'},
         {color: '#8b54aa', colorName: 'red', price: '100'}
       ];
       this.prices = [];
