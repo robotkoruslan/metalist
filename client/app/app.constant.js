9bfff9c2770674e4cb0f3e2b8a345116bedc4865
Alex <polyakov_as@ukr.net>
2017-07-16 23:44:16 +0300

    fixSB

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -2432,6 +2432,45 @@ let constantsModule = angular.module("metalistTicketsApp.constants", [])
           }
         ]
       },
+      sector_SB_2: {
+        name: 'SB_1',
+        rows: [
+          {
+            name: '1',
+            seats: '7'
+          },
+          {
+            name: '2',
+            seats: '7'
+          }
+        ]
+      },
+      sector_SB_3_5: {
+        name: 'SB_1',
+        rows: [
+          {
+            name: '1',
+            seats: '7'
+          },
+          {
+            name: '2',
+            seats: '7'
+          }
+        ]
+      },
+      sector_SB_6: {
+        name: 'SB_1',
+        rows: [
+          {
+            name: '1',
+            seats: '7'
+          },
+          {
+            name: '2',
+            seats: '7'
+          }
+        ]
+      },
       sector_SB_7: {
         name: 'SB_7',
         rows: [
@@ -2442,6 +2481,58 @@ let constantsModule = angular.module("metalistTicketsApp.constants", [])
           {
             name: '2',
             seats: '5'
+          }
+        ]
+      },
+      sector_SB_8: {
+        name: 'SB_1',
+        rows: [
+          {
+            name: '1',
+            seats: '7'
+          },
+          {
+            name: '2',
+            seats: '7'
+          }
+        ]
+      },
+      sector_SB_9: {
+        name: 'SB_1',
+        rows: [
+          {
+            name: '1',
+            seats: '7'
+          },
+          {
+            name: '2',
+            seats: '7'
+          }
+        ]
+      },
+      sector_SB_10: {
+        name: 'SB_1',
+        rows: [
+          {
+            name: '1',
+            seats: '7'
+          },
+          {
+            name: '2',
+            seats: '7'
+          }
+        ]
+      },
+      sector_SB_11: {
+        name: 'SB_1',
+        rows: [
+          {
+            name: '1',
+            seats: '7'
+          },
+          {
+            name: '2',
+            seats: '7'
           }
         ]
       }
