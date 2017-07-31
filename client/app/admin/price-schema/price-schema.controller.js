3f86dbcb5ec246362bf0a8e2ac7aecac687352c6
Alex <polyakov_as@ukr.net>
2017-07-31 20:32:24 +0300

    bardak

--- client/app/admin/price-schema/price-schema.controller.js
+++ client/app/admin/price-schema/price-schema.controller.js
@@ -14,7 +14,7 @@ export default class PriceSchemaController {
     this.currentSector = {};
     this.currentSector.color = '#808080';
     this.message = '';
-    this.priceSchemaId = '';
+    //this.priceSchemaId = '';
     this.colors = [
       {color: '#8b54aa', colorName: 'violet'},
       {color: '#ffcc00', colorName: 'yellow'},
@@ -22,6 +22,9 @@ export default class PriceSchemaController {
       {color: '#54aa6a', colorName: 'green'},
       {color: '#ff972f', colorName: 'orange'}
     ];
+    this.currentColorSchema = {};
+    this.currentTribuneColor = {};
+    this.currentSectorColor = {};
 
   }
 
@@ -37,6 +40,7 @@ export default class PriceSchemaController {
   }
 
   setCurrentPriceSchema(schemaName) {
+    console.log('setCurrentPriceSchema', schemaName);
     this.currentTribune = {};
     this.currentSector = {};
 
@@ -46,7 +50,27 @@ export default class PriceSchemaController {
       let currentPriceSchema = this.priceSchemas.filter(schema => schema.priceSchema.name === schemaName)[0];
       this.priceSchemaId = currentPriceSchema.id;
       this.currentPriceSchema = currentPriceSchema.priceSchema;
+      this.currentColorSchema = currentPriceSchema.priceSchema.colorSchema;
+      console.log('currentPriceSchema', currentPriceSchema);
+      console.log('this.currentColorSchema', this.currentColorSchema);
     }
+  }
+
+  setCurrentColorSchema(color, price) {
+console.log('setCurrentColorSchema',color, price);
+
+
+    if (!color) {
+      console.log('!color',color, this.currentColorSchema.indexOf(price));
+      console.log('!color currentColorSchema', this.currentColorSchema);
+
+      this.currentColorSchema.indexOf(price);}
+
+    console.log('!color currentColorSchema', this.currentColorSchema);
+    console.log('!color currentColorSchema', this.currentColorSchema);
+
+    console.log('color', this.currentColorSchema.indexOf(color.color));
+    //this.currentColorSchema
   }
 
   getSectorForSetPrice($event) {
@@ -84,9 +108,18 @@ export default class PriceSchemaController {
   getPreparedPriceSchema(tribuneName, sectorNumber) {
     let priceSchema = this.currentPriceSchema,
       tribune = this.currentTribune,
-      sector = this.currentSector;
+      sector = this.currentSector,
+      tribuneColor = this.currentTribuneColor,
+      sectorColor = this.currentSectorColor;
+
+    if (!priceSchema.colorSchema) {priceSchema.colorSchema = []}
 
     delete sector.rows;
+    let colorPriceTribune = {price : tribune.price, color: tribuneColor},
+        colorPriceSector = {price : sector.price, color: sectorColor};
+
+    priceSchema.colorSchema.push(colorPriceTribune);
+    priceSchema.colorSchema.push(colorPriceSector);
 
     if (priceSchema['tribune_' + tribuneName] && priceSchema['tribune_' + tribuneName]['sector_' + sectorNumber]) {
 
@@ -110,6 +143,14 @@ export default class PriceSchemaController {
   }
 
   savePriceSchema(form, tribuneName, sectorNumber) {
+    console.log('form', form);
+    console.log('this.currentColorSchema', this.currentColorSchema);
+    console.log('tribuneName', tribuneName);
+    console.log('sectorNumber', sectorNumber);
+    console.log('$ctrl.currentTribune', this.currentTribune);
+    console.log('$ctrl.currentSector', this.currentSector);
+    console.log('currentTribuneColor', this.currentTribuneColor);
+    console.log('currentSectorColor', this.currentSectorColor);
     form.$setDirty();
     form.schemaName.$setDirty();
     form.tribunePrice.$setDirty();
@@ -122,7 +163,7 @@ export default class PriceSchemaController {
 
     if (form.$valid) {
       let priceSchema = this.getPreparedPriceSchema(tribuneName, sectorNumber);
-
+console.log('getPreparedPriceSchema priceSchema', priceSchema);
       this.priceSchemaService.savePriceSchema(priceSchema)
         .then(response => {
           this.currentPriceSchema = response.data.priceSchema;
