4e62e4e0a78fdb01000eec3281790c2f6af69ccc
VladRud <rudslawa@gmail.com>
2018-06-15 19:03:23 +0300

    add ssl

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,20 +3,20 @@
 // Production specific configuration
 // =================================
 module.exports = {
-    // Server IP
-    ip: process.env.OPENSHIFT_NODEJS_IP ||
-    process.env.IP ||
-    undefined,
+  // Server IP
+  ip: process.env.OPENSHIFT_NODEJS_IP ||
+  process.env.IP ||
+  undefined,
 
-    // Server port
-    port: process.env.OPENSHIFT_NODEJS_PORT ||
-    process.env.PORT ||
-    8080,
+  // Server port
+  port: process.env.OPENSHIFT_NODEJS_PORT ||
+  process.env.PORT ||
+  80,
 
   // MongoDB connection options
   mongo: {
     uri: 'mongodb://' + (process.env.MONGO_1_PORT_27017_TCP_ADDR || 'mongo') + ':27017/metalisttickets'
   }
-    // MongoDB connection options
+  // MongoDB connection options
 
 };
